f06e6629ab803b6f03b026673dc1e7ea86f76537
sirajlubis <mantapdjiwa6@gmail.com>
2024-02-12 10:45:45 +0700

    Mengubah angka menjadi bil

--- Kalkulator.java
+++ Kalkulator.java
@@ -7,7 +7,7 @@ public class Kalkulator {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int angka1, angka2;
+        int bil1, bil2;
 
         System.out.println("Pilih operasi: ");
         System.out.println("1. Penjumlahan");
@@ -20,19 +20,19 @@ public class Kalkulator {
         String pilihan = scanner.nextLine();
 
         if (pilihan.equals("1") || pilihan.equals("2") || pilihan.equals("3") || pilihan.equals("4")) {
-            System.out.println("Masukkan bilangan pertama: ");
-            angka1 = scanner.nextInt();
-            System.out.println("Masukkan bilangan kedua: ");
-            angka2 = scanner.nextInt();
+            System.out.println("Masukkan bil pertama: ");
+            bil1 = scanner.nextInt();
+            System.out.println("Masukkan bil kedua: ");
+            bil2 = scanner.nextInt();
 
             if (pilihan.equals("1")) {
-                System.out.println(angka1 + " + " + angka2 + " = " + (angka1 + angka2));
+                System.out.println(bil1 + " + " + bil2 + " = " + (bil1 + bil2));
             } else if(pilihan.equals("2")) {
-                System.out.println(angka1 + " - " + angka2 + " = " + (angka1 - angka2));
+                System.out.println(bil1 + " - " + bil2 + " = " + (bil1 - bil2));
             } else if(pilihan.equals("3")) {
-                System.out.println(angka1 + "x" + angka2 + " = " + (angka1 * angka2));
+                System.out.println(bil1 + "x" + bil2 + " = " + (bil1 * bil2));
             } else if(pilihan.equals("4")) {
-                System.out.println(angka1 + "/" + angka2 + " = " + (angka1 / angka2));
+                System.out.println(bil1 + "/" + bil2 + " = " + (bil1 / bil2));
             }
         }else{
             System.out.println("Pilihan invalid");
